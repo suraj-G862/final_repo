0e9e688449cb8d220efd48167059a0defb771bf1
OmAnand857 <allansmith2561@gmail.com>
2024-03-15 03:18:59 +0530

    All pages almost Responsive..

--- prayatna/src/components/Header.jsx
+++ prayatna/src/components/Header.jsx
@@ -1,51 +1,59 @@
-import React from "react";
+import React,{useState} from "react";
 import "./styles/header.css";
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
 import {Link }from "react-router-dom";
 
 
 function Header(){
+
+                const [clicked,setClicked]=useState(false);
+
+
+
+                function handleClick(){
+                                setClicked(!clicked);
+                }
+
             return (
                        <div className="Navbar_Container">
 
                     <Link to="/" ><div className="Navbar_Logo">
-                                        <img src="./prayatna_01.png" alt="" />
+                                        <img src="./prayatna_01.png" alt=""></img>
                                         <img src="./PrayatnaLogo.jpg" alt=""></img>
                                 </div>
                     </Link>
 
-                                <div className="Navbar_Options">
+                                <div className={clicked?'Navbar_Options active':'Navbar_Options'}>
 
-                                        <Link to="/" style={{ textDecoration: 'none' }}><div className="Options Navbar_Home">
+                                        <Link to="/" style={{ textDecoration: 'none' }}><div onClick={()=>{setClicked(!clicked)}} className="Options Navbar_Home">
                                                     <p>Home</p>
                                                 </div>
                                             </Link>
-                                            <Link to="/Photos" style={{ textDecoration: 'none' }}>
-                                    <div className="Options Navbar_Photos">
+
+                                   <Link to="/Photos" onClick={()=>{setClicked(!clicked)}} style={{ textDecoration: 'none' }}><div className="Options Navbar_Photos">
                                                     <p>Photos</p>
                                     </div>
-                                    </Link>
-                                    <Link to="/Stats" style={{textDecoration:'none'}}>
-                                        <div className="Options Navbar_Stats">
+                                    </Link> 
+                        <Link to="/Stats" onClick={()=>{setClicked(!clicked)}} style={{ textDecoration: 'none' }}>
+                                    <div className="Options Navbar_Stats">
                                             <p>Stats</p>
                                     </div>
-                                    </Link>
-                                    
-                    <Link to="/Teams" style={{ textDecoration: 'none' }}><div className="Options Navbar_Teams">
+                                </Link>
+                    <Link to="/Teams" onClick={()=>{setClicked(!clicked)}} style={{ textDecoration: 'none' }}><div className="Options Navbar_Teams">
                                             <p>Teams</p>
                                     </div>
                     </Link>
-
-                    <Link to="/Standings" style={{ textDecoration: 'none' }}>
-                                    <div className="Options Navbar_Standings">
+                                  <Link to="/Standings" onClick={()=>{setClicked(!clicked)}} style={{ textDecoration: 'none' }}>  <div className="Options Navbar_Standings">
                                             <p>Standings</p>
                                     </div>
-                        </Link>
-                        <Link to="/Matches" style={{ textDecoration: 'none' }}>
+                                    
+                                    </Link>
+                                <Link to="/Matches" onClick={()=>{setClicked(!clicked)}} style={{ textDecoration: 'none' }}>
                                     <div className="Options Navbar_Home_Matches">
                                             <p>Matches</p>
                                     </div>
-                                    
                                 </Link>
 
                                 </div>
@@ -53,6 +61,9 @@ function Header(){
                         <div className="Navbar_SearchBar">
                             <input type="text" className="Header_Search" placeholder="Search Prayatna"/>
                             <SearchIcon className="Header_SearchIcon"/>
+                        </div>
+                        <div className="Navbar_Buttons">
+                                       {clicked?<CloseIcon onClick={handleClick} className="Navbar_Close_Icon"/>:<MenuIcon onClick={handleClick} className="Navbar_Menu_Icon"/>}
                         </div>
                     
 
--- prayatna/src/components/photos.jsx
+++ prayatna/src/components/photos.jsx
@@ -24,7 +24,7 @@ const photos = [
 
 function PhotosGallery() {
   return (
-    <>
+    <div className="photos_container">
     <h1 className="title_photo">Get a glimpse of fire in our Veins</h1>
     <div class="container_photo">
       <div class="column_photo">
@@ -62,7 +62,7 @@ function PhotosGallery() {
         <div class="small-image_photo"><img src="https://images.unsplash.com/photo-1599305445671-ac291c95aaa9?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="image_ins" /></div>
       </div>
     </div>
-    </>
+    </div>
   );
 }
 
