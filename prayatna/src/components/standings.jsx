e08ee56278b84175fb29837f0f537d476fc41326
Suraj <surajutt@iitbhilai.ac.in>
2024-03-15 18:50:54 +0530

    standings page updated

--- prayatna/src/components/standings.jsx
+++ prayatna/src/components/standings.jsx
@@ -3,14 +3,22 @@ import "./styles/standings.css";
 
 function PrayatnaRanking() {
   const [teams, setTeams] = useState([
-    { rank: 1, name: 'Snow Leopards', total: 99, f: 2, a: 4, gd: 12 },
-    { rank: 2, name: 'Snow Leopards', total: 99, f: 2, a: 4, gd: 12 },
-    { rank: 3, name: 'Snow Leopards', total: 99, f: 2, a: 4, gd: 12 },
-    { rank: 4, name: 'Snow Leopards', total: 99, f: 2, a: 4, gd: 12 },
-    { rank: 5, name: 'Snow Leopards', total: 99, f: 2, a: 4, gd: 12 },
-    { rank: 6, name: 'Snow Leopards', total: 99, f: 2, a: 4, gd: 12 },
-    { rank: 7, name: 'Snow Leopards', total: 99, f: 2, a: 4, gd: 12 },
-    { rank: 8, name: 'Snow Leopards', total: 99, f: 2, a: 4, gd: 12 }
+    // { rank: 1, name: 'Snow Leopards', total: 16, f: 2, a: 4, gd: 12 },
+    // { rank: 2, name: 'Gladiators', total: 99, f: 2, a: 4, gd: 12 },
+    // { rank: 3, name: 'Warlords', total: 99, f: 2, a: 4, gd: 12 },
+    // { rank: 4, name: 'Desert Hawks', total: 99, f: 2, a: 4, gd: 12 },
+    // { rank: 5, name: 'Silver Falcons', total: 99, f: 2, a: 4, gd: 12 },
+    // { rank: 6, name: 'Tridents', total: 99, f: 2, a: 4, gd: 12 },
+    // { rank: 7, name: 'Black Archers', total: 99, f: 2, a: 4, gd: 12 },
+    // { rank: 8, name: 'Firebirds', total: 99, f: 2, a: 4, gd: 12 }
+    { rank: 1, name: 'Snow Leopards', total: 16 },
+    { rank: 2, name: 'Gladiators', total: 15 },
+    { rank: 3, name: 'Warlords', total: 15 },
+    { rank: 4, name: 'Desert Hawks', total: 15 },
+    { rank: 5, name: 'Silver Falcons', total: 15 },
+    { rank: 6, name: 'Tridents', total: 14 },
+    { rank: 7, name: 'Black Archers', total: 12 },
+    { rank: 8, name: 'Firebirds', total: 6 }
   ]);
 
   return (
@@ -26,9 +34,9 @@ function PrayatnaRanking() {
               <th width="10%" align="left">RANKING</th>
               <th width="45%" align="left">TEAM</th>
               <th width="15%" align="left">TOTAL</th>
-              <th width="15%" align="left">F</th>
+              {/* <th width="15%" align="left">F</th>
               <th width="15%" align="left">A</th>
-              <th width="15%" align="left">GD</th>
+              <th width="15%" align="left">GD</th> */}
             </tr>
           </thead>
           <tbody>
@@ -37,9 +45,9 @@ function PrayatnaRanking() {
                 <td>{team.rank}</td>
                 <td>{team.name}</td>
                 <td>{team.total}</td>
-                <td>{team.f}</td>
+                {/* <td>{team.f}</td>
                 <td>{team.a}</td>
-                <td>{team.gd}</td>
+                <td>{team.gd}</td> */}
               </tr>
             ))}
           </tbody>
